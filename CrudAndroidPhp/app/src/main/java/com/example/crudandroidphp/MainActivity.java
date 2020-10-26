782a586fe253d466687e1c6a34a02a740c0652b7
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-10-25 22:09:22 -0300

    Feito inicializacao da UI

--- CrudAndroidPhp/app/src/main/java/com/example/crudandroidphp/MainActivity.java
+++ CrudAndroidPhp/app/src/main/java/com/example/crudandroidphp/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.crudandroidphp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editNome, editTelefone, editEmail, editId;
+    Button btnNovo, btnSalvar, btnExcluir;
+    ListView listViewContatos;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editId = findViewById(R.id.editId);
+        editNome = findViewById(R.id.editNome);
+        editTelefone = findViewById(R.id.editTelefone);
+        editEmail = findViewById(R.id.editEmail);
+
+        btnNovo = findViewById(R.id.btnNovo);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnExcluir = findViewById(R.id.btnExcluir);
+
+        listViewContatos= findViewById(R.id.listVewContato);
     }
 }
