5c7945bfdf314e2cceea2a8d2f2cb7719def0384
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-10-25 22:18:53 -0300

    Configurado evento do btnSalvar que testa se o id e nome nao estoa vazios

--- CrudAndroidPhp/app/src/main/java/com/example/crudandroidphp/MainActivity.java
+++ CrudAndroidPhp/app/src/main/java/com/example/crudandroidphp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -30,5 +31,22 @@ public class MainActivity extends AppCompatActivity {
         btnExcluir = findViewById(R.id.btnExcluir);
 
         listViewContatos= findViewById(R.id.listVewContato);
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = editId.getText().toString();
+                String nome = editNome.getText().toString();
+                String telefone = editTelefone.getText().toString();
+                String email = editEmail.getText().toString();
+
+                if (nome.isEmpty()){
+                    editNome.setError("O nome é obrigatório");
+                } else if (id.isEmpty()){
+                    
+                }
+            }
+        });
     }
+
 }
