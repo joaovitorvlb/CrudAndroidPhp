cb8c89925e6195d9de67f99c2e836d096ee5b9c5
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-10-25 23:22:37 -0300

    Configurado evento do botao salvae e envio pos com lib ION

--- CrudAndroidPhp/app/src/main/java/com/example/crudandroidphp/MainActivity.java
+++ CrudAndroidPhp/app/src/main/java/com/example/crudandroidphp/MainActivity.java
@@ -8,12 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editNome, editTelefone, editEmail, editId;
     Button btnNovo, btnSalvar, btnExcluir;
     ListView listViewContatos;
+
+    private String HOST = "http://192.168.0.16/crud";
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -35,15 +42,35 @@ public class MainActivity extends AppCompatActivity {
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String id = editId.getText().toString();
                 String nome = editNome.getText().toString();
                 String telefone = editTelefone.getText().toString();
                 String email = editEmail.getText().toString();
 
+                String url = HOST + "/create.php";
+
                 if (nome.isEmpty()){
                     editNome.setError("O nome é obrigatório");
                 } else if (id.isEmpty()){
-                    
+                    Ion.with(MainActivity.this)
+                            .load(url)
+                            .setBodyParameter("nome", nome)
+                            .setBodyParameter("telefone", telefone)
+                            .setBodyParameter("email", email)
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+                                    if(result.get("create").getAsString().equals("ok")) {
+
+                                        // int idRetornado = Integer.parseInt(result.get("id").getAsString());
+                                        Toast.makeText(MainActivity.this, "Salvo com sucesso", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Ocorreu erro na hora de gravar", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
                 }
             }
         });
